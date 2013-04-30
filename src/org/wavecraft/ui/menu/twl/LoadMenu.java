d33065e973003a2da2a3bcf283402065fe005408
laurentsifre <laurent.sifre@gmail.com>
2013-04-30 11:16:52 +0200

    todo load menu with all loaded saved game

---
+++ src/org/wavecraft/ui/menu/twl/LoadMenu.java
@@ -0,0 +1,9 @@
+package org.wavecraft.ui.menu.twl;
+
+import de.matthiasmann.twl.BoxLayout;
+import de.matthiasmann.twl.BoxLayout.Direction;
+
+public class LoadMenu {
+
+	private BoxLayout boxLayout = new BoxLayout(Direction.VERTICAL);
+}
